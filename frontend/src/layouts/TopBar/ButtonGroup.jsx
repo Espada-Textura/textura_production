33ca2285533ebbfe5398902be933313c9aceecce
hoklenghak <hoklenghak168@gmail.com>
2022-11-12 20:41:50 +0700

    added secondary colour button styles

--- frontend/src/layouts/TopBar/ButtonGroup.jsx
+++ frontend/src/layouts/TopBar/ButtonGroup.jsx
@@ -9,23 +9,23 @@ import {
 export default function ButtonGroup() {
   return (
     <div className={"flex gap-4"}>
-      <button className={"btn-filled btn-large"}>
+      <button className={"btn-filled-sec btn-large"}>
         <HiUpload className={"w-6 h-6"} />
         Upload
       </button>
 
-      <button className={"btn-plain-filled icon-btn-large"}>
+      <button className={"btn-plain-filled-sec icon-btn-large"}>
         <HiBell className={"w-6 h-6"} />
       </button>
 
-      <button className={"btn-plain-filled icon-btn-large"}>
+      <button className={"btn-plain-filled-sec icon-btn-large"}>
         <HiPaperAirplane
           className={"w-6 h-6 rotate-45 translate-x-0.5 -translate-y-0.5"}
         />
       </button>
 
       <div className={"profile-btn"}>
-        <button className={"icon-btn-large btn-plain-filled"}>
+        <button className={"icon-btn-large btn-plain-filled-sec"}>
           {/* TODO: Conditional Rendering on the profile*/}
           <HiUser className={"w-6 h-6"} />
         </button>
