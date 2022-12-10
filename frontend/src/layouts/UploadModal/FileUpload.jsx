18b5b1ebda970fbb30b19da05cc2270b35ad4868
hoklenghak <hoklenghak168@gmail.com>
2022-12-10 16:36:42 +0700

    [sidemenu] - [profile] font size change

--- frontend/src/layouts/UploadModal/FileUpload.jsx
+++ frontend/src/layouts/UploadModal/FileUpload.jsx
@@ -1,8 +1,23 @@
+import TextareaAutoSize from "react-textarea-autosize";
+
+import { useDropzone } from "react-dropzone";
+
 const FileUpload = () => {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone();
+
   return (
     <>
-      <div className={""}>
-        <input name={"upload-art-desc"} type={"text"} />
+      <div className={"upload-art-portal"}>
+        <TextareaAutoSize placeholder={"What do you think of your work?"} />
+        <div {...getRootProps({ className: "upload-dropzone" })}>
+          <input {...getInputProps()} />
+          <img src="/src/images/cloud.svg" alt="cloud" />
+          <span>
+            Drag and drop your artwork here, or click <span>Browse</span>
+          </span>
+          <span>Accept any image format.</span>
+          <span>Size Limit : 10MB for one picture</span>
+        </div>
       </div>
     </>
   );
--- frontend/src/layouts/UploadModal/index.jsx
+++ frontend/src/layouts/UploadModal/index.jsx
@@ -6,39 +6,8 @@ const uploadModal = (props) => {
 
   return (
     <>
-      <div className={"upload-container"}>
-        <div className={"upload-title"}>
-          <h2>Upload Artwork</h2>
-          <span>
-            Let our community know about your creation, or share us what is your
-            working on.
-          </span>
-        </div>
-        <form>
-          <input
-            name={"upload-art-name"}
-            type={"text"}
-            placeholder={"Name of this masterpiece"}
-          />
-          <FileUpload />
-          <div className={"upload-art-buttons"}>
-            <button
-              className={"button-fair-accent button-medium"}
-              // onClick={}
-            >
-              Cancel
-            </button>
-            <div className={"upload-art-p-buttons"}>
-              <button className={"button-plain-accent button-medium"}>
-                Save to Draft
-              </button>
-              <button className={"button-contained-accent button-medium"}>
-                Upload
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+      <div className={"upload-portal"}></div>
+      <div className={"upload-container"}></div>
     </>
   );
 };
