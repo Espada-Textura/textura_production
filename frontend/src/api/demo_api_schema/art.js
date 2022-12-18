353fb2fcba1f9050f7abfa25fcaa90df7361ec55
NirPisatto <misapisatto@gmail.com>
2022-12-18 22:52:16 +0700

    [api demo schema] - Added API demo schema

---
+++ frontend/src/api/demo_api_schema/art.js
@@ -0,0 +1,102 @@
+let postArt = {
+  "title" : "art name",
+  "description" : "none",
+  "images" : [
+    {
+      "description" : "none",
+      "index" : "0",
+      "image" : {
+        "mime": "image/jpeg",
+        "data": "image_data"
+      }
+    },
+    {
+      "description" : "none",
+      "index" : "1",
+      "image" : {
+        "mime": "image/jpeg",
+        "data": "image_data"
+      }
+    }
+  ]
+}
+
+
+let get_art= {
+    "title": "art",
+    "aid": "GeaEr56SfNPkXXaPQezKbp",
+    "description": "none",
+    "id": 2,
+    "like": 0,
+    "view": 0,
+    "image_count" : 2,
+    "createdDate": "07-12-2022 19:40",
+    "updatedDate": "07-12-2022 19:41",
+    "user": {
+        "createdDate": "07-12-2022 17:21",
+        "email": "misapisatto@gmail.com",
+        "firstName": "MISA",
+        "id": 1,
+        "lastName": "Pisatto",
+        "uid": "ZNpKTCMgHYVh6xbyw5BpVS",
+        "updatedDate": "07-12-2022 17:21",
+        "username": ""
+    },
+    "images": [{
+      "index" : 0,
+      "description": "none",
+      "preimage": "VFE:0npK?cM}-=NDDhx[xtMwE0IUD%ozRP_N-;jb%fXT",
+      "rpath": "web.textura-art.com/api/art/GeaEr56SfNPkXXaPQezKbp.jpeg",
+      "status": "completed",
+      "tpath": "web.textura-art.com/api/art/GeaEr56SfNPkXXaPQezKbp_thumbnail.jpeg",
+      "updatedDate": "07-12-2022 19:41",
+      "createdDate": "07-12-2022 19:40",
+    },
+    { 
+      "index" : 1,
+      "description": "none",
+      "preimage": "VFE:0npK?cM}-=NDDhx[xtMwE0IUD%ozRP_N-;jb%fXT",
+      "rpath": "web.textura-art.com/api/art/GeaEr56SfNPkXXaPQezKbp.jpeg",
+      "status": "completed",
+      "tpath": "web.textura-art.com/api/art/GeaEr56SfNPkXXaPQezKbp_thumbnail.jpeg",
+      "updatedDate": "07-12-2022 19:41",
+      "createdDate": "07-12-2022 19:40",
+    }]
+}
+
+let get_arts = {
+  "page": 2,
+  "perPage": 2,
+  "totalPages": 2,
+  "totalRecords": 3,
+  "art" :[{
+    "title": "art",
+    "aid": "GeaEr56SfNPkXXaPQezKbp",
+    "description": "none",
+    "id": 2,
+    "like": 0,
+    "view": 0,
+    "image_count" : 2,
+    "createdDate": "07-12-2022 19:40",
+    "updatedDate": "07-12-2022 19:41",
+    "user": {
+        "createdDate": "07-12-2022 17:21",
+        "email": "misapisatto@gmail.com",
+        "firstName": "MISA",
+        "id": 1,
+        "lastName": "Pisatto",
+        "uid": "ZNpKTCMgHYVh6xbyw5BpVS",
+        "updatedDate": "07-12-2022 17:21",
+        "username": ""
+    },
+    "images": [{
+      "index" : 0,
+      "description": "none",
+      "preimage": "VFE:0npK?cM}-=NDDhx[xtMwE0IUD%ozRP_N-;jb%fXT",
+      "rpath": "web.textura-art.com/api/art/GeaEr56SfNPkXXaPQezKbp.jpeg",
+      "status": "completed",
+      "tpath": "web.textura-art.com/api/art/GeaEr56SfNPkXXaPQezKbp_thumbnail.jpeg",
+      "updatedDate": "07-12-2022 19:41",
+      "createdDate": "07-12-2022 19:40",
+    }]
+}]}
