fe80ab54c2369c1642e4e80413fbf89f36c5223e
YornChanvisal <chanvisalyorn@gmail.com>
2022-12-22 14:10:48 +0700

    [Sign Up] - Render Done

--- frontend/src/containers/SignUp/SignUp.jsx
+++ frontend/src/containers/SignUp/SignUp.jsx
@@ -1,49 +1,62 @@
-import Logo from "@components/Logo";
-
+import logoBlackSvg from "@images/logo.svg";
 import logoWhiteSvg from "@/images/logoWhite.svg";
 
 import { FcGoogle } from "react-icons/fc";
 
 const SignUp = () => {
   return (
-    <div className="signUp-container bg-[url(/src/images/sign-up.png)] bg-auto bg-no-repeat bg-center w-full h-screen">
-      <div className="signUp-content h-screen backdrop-blur-lg md:flex md:flex-col">
-        <div className="signUp--form">
-          <form className="signUp--form-form text-center">
-            <div className="signUp--logo flex justify-center">
-              <img src={logoWhiteSvg} alt="logo" draggable={false} />
+    <div className="signUp-container bg-[url(/src/images/sign-up.png)] bg-auto bg-no-repeat bg-center w-full h-screen md:p-14 flex justify-center items-center">
+      <div className="signUp-content backdrop-blur-xl md:rounded-2xl lg:flex lg:flex-row">
+        <div className="signUp--form p-14 rounded-l-2xl max-md:h-screen lg:w-[50%] lg:bg-primary-100 xl:px-24 2xl:px-36">
+          <form className="signUp--form-form text-center flex flex-col">
+            <div className="signUp--logo flex justify-center pt-5">
+              <picture>
+                <source media="(min-width: 1024px)" srcset={logoBlackSvg} />
+                <img src={logoWhiteSvg} alt="logo" draggable="false" />
+              </picture>
             </div>
             <div className="signUp--notes">
-              <div className="signUp--notes--main-text mx-12">
+              <div className="signUp--notes--main-text mt-4 lg:text-secondary-100">
                 Create an account
               </div>
-              <div className="signUp--note--small-text mx-12">
+              <div className="signUp--note--small-text mt-2 lg:text-secondary-70">
                 Welcome to our neighbourhood. Let's get you started.
               </div>
             </div>
-            <div className="signUp--alternative-signin flex justify-center mx-14">
-              <button type="button" className="sign-in--google w-full h-14">
+            <div className="signUp--alternative-signin flex justify-center my-5">
+              <button
+                type="button"
+                className="sign-in--google w-full h-14 lg:text-secondary-100 lg:border-2 lg:border-solid lg:border-secondary-30"
+              >
                 <FcGoogle size={30} />
                 Sign in with Google
               </button>
             </div>
-            <div className="signUp--or-statement">- or -</div>
-            <div className="signUp--fills mx-14">
-              <div className="flex justify-between">
-                <div className="flex flex-col text-left">
-                  <label for="fname">First Name</label>
+            <div className="signUp--or-statement flex justify-center items-center mb-6">
+              <div className="signUp--horizontal-line bg-primary-100 lg:bg-secondary-30 mr-4"></div>
+              <div className="lg:lg:text-secondary-50">or</div>
+              <div className="signUp--horizontal-line bg-primary-100 lg:bg-secondary-30 ml-4"></div>
+            </div>
+            <div className="signUp--fills mb-8">
+              <div className="signUp--fill--name flex justify-between mb-5">
+                <div className="signUp--fill--fname flex flex-col text-left w-[49%]">
+                  <label className="lg:text-secondary-100" for="fname">
+                    First Name
+                  </label>
                   <input
-                    className="h-14 bg-primary-10/0 placeholder-[#F9FBFC]/70 border-2 border-primary-100 text-primary-100 text-sm rounded-lg focus:ring-accent-100 focus:border-accent-100 p-2.5"
+                    className="signUp--inputs h-14 placeholder-[#F9FBFC]/70 text-primary-100 text-sm rounded-2xl p-3 lg:text-secondary-30 lg:placeholder-secondary-50 lg:border-2 lg:border-solid lg:border-secondary-30"
                     id="fname"
                     type="text"
                     placeholder="Jonh"
                     required
                   ></input>
                 </div>
-                <div className="flex flex-col text-left">
-                  <label for="fname">Last Name</label>
+                <div className="signUp--fill--lname flex flex-col text-left w-[49%]">
+                  <label className="lg:text-secondary-100" for="lname">
+                    Last Name
+                  </label>
                   <input
-                    className="h-14 bg-primary-10/0 placeholder-[#F9FBFC]/70 border-2 border-primary-100 text-primary-100 text-sm rounded-lg focus:ring-accent-100 focus:border-accent-100 p-2.5"
+                    className="signUp--inputs h-14 placeholder-[#F9FBFC]/70 text-primary-100 text-sm rounded-2xl p-3 lg:text-secondary-100 lg:placeholder-secondary-50 lg:border-2 lg:border-solid lg:border-secondary-30"
                     id="lname"
                     type="text"
                     placeholder="Doe"
@@ -51,10 +64,12 @@ const SignUp = () => {
                   ></input>
                 </div>
               </div>
-              <div className="flex flex-col text-left">
-                <label for="email">Email</label>
+              <div className="signUp--fill--email flex flex-col text-left">
+                <label className="lg:text-secondary-100" for="email">
+                  Email
+                </label>
                 <input
-                  className="h-14 bg-primary-10/0 placeholder-[#F9FBFC]/70 border-2 border-primary-100 text-primary-100 text-sm rounded-lg focus:ring-accent-100 focus:border-accent-100 p-2.5"
+                  className="signUp--inputs h-14 placeholder-[#F9FBFC]/70 text-primary-100 text-sm rounded-2xl p-3 lg:text-secondary-100 lg:placeholder-secondary-50 lg:border-2 lg:border-solid lg:border-secondary-30"
                   id="email"
                   type="email"
                   placeholder="example@example.com"
@@ -62,7 +77,7 @@ const SignUp = () => {
                 ></input>
               </div>
             </div>
-            <div className="signUp--submit flex justify-center mx-14">
+            <div className="signUp--submit flex justify-center mb-8">
               <button
                 className="signUp--submit-button w-full h-14"
                 type="button"
@@ -70,33 +85,33 @@ const SignUp = () => {
                 Next
               </button>
             </div>
-            <div>Already have an account? <a className="">Sign In</a></div>
+            <div className="signUp--has-account mb-8 lg:text-secondary-70">
+              Already have an account?{" "}
+              <a className="text-accent-100">Sign In</a>
+            </div>
+            <div className="signUp--agree-term mb-8 lg:text-secondary-70">
+              You acknowledge that you have read and agree to our{" "}
+              <a className="text-accent-100">terms and conditions</a> and{" "}
+              <a className="text-accent-100">privacy policy</a>.
+            </div>
           </form>
         </div>
 
-        <div className="signUp--decor-content hidden md:flex">
-          <div className="signUp--decor-content-main">
-            One of the
-            <br />
-            largest artistic
-            <br />
-            communities
-            <br />
-            in Cambodia.
+        <div className="signUp--decor-content hidden p-14 lg:w-[50%] lg:flex lg:flex-col lg:justify-end lg:content-end">
+          <div className="signUp--decor-content-main text-right">
+            One Of The Largest Artistic Platform In Cambodia.
           </div>
-          <div className="signUp--decor-content-small">
-            Most persons who engage in artistic and cultural pursuits
-            <br />
-            would agree that they improve the quality of their lives
-            <br />
-            because they provide possibilities for social interaction,
-            <br />
-            personal fulfillment, enlightening viewpoints, and intellectual
-            <br />
-            stimulation.
+          <div className="signUp--decor-content-small text-right">
+            Most persons who engage in artistic and cultural pursuits would
+            agree that they improve the quality of their lives because they
+            provide possibilities for social interaction, personal fulfillment,
+            enlightening viewpoints, and intellectual stimulation.
           </div>
         </div>
       </div>
+      {/* <div className="signUp--background-credit text-center">
+        Photo by <a>Filip Mroz</a> on <a>Unsplash</a>
+      </div> */}
     </div>
   );
 };
