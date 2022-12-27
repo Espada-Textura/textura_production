2149aafe7d2f61132815973b73baf2d6143efdeb
hoklenghak <hoklenghak168@gmail.com>
2022-12-27 14:02:11 +0700

    [api] ~ cors header

--- frontend/src/api/art.api.jsx
+++ frontend/src/api/art.api.jsx
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const artApi = axios.create({
   baseURL: "https://web.textura-art.com/api/",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept",
+  },
 });
 
 export default artApi;
