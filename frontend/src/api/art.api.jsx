72a4b8234c44a1a81f3d16c53c72e40282c61dce
hoklenghak <hoklenghak168@gmail.com>
2022-12-27 17:41:28 +0700

    [api] post request testing

--- frontend/src/api/art.api.jsx
+++ frontend/src/api/art.api.jsx
@@ -1,13 +1,9 @@
 import axios from "axios";
 
 const artApi = axios.create({
-  baseURL: "https://web.textura-art.com/api/",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers":
-      "Origin, X-Requested-With, Content-Type, Accept",
-  },
+  baseURL: "https://web.textura-art.com/api",
+  AccessControlAllowOrigin: "*",
+  AccessControlAllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 });
 
 export default artApi;
