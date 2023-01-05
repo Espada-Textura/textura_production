cd4398c5602d764246d042c26e8012dbce55e5da
NirPisatto <misapisatto@gmail.com>
2023-01-05 14:14:26 +0700

    [html header] - Added healmet

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,7 @@
 //react
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 //styles
 import "@/sass/index.scss";
@@ -14,9 +15,23 @@ import App from "@/containers/App/App.jsx";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </QueryClientProvider>
+  <HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Helmet>
+          <title>Hello</title>
+          <link rel="canonical" href="https://web.textura-art.com/" />
+          <meta
+            property="twitter:image"
+            content="https://web.textura-art.com/api/art/Jqiu3gLgwzNg9mUG6BEv9k_thumbnail.jpeg"
+          />
+          <meta
+            property="og:image"
+            content="https://web.textura-art.com/api/art/Jqiu3gLgwzNg9mUG6BEv9k_thumbnail.jpeg"
+          />
+        </Helmet>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </HelmetProvider>
 );
