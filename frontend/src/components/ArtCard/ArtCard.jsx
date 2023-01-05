0fc9b24c4f37f0e1d6c296c791d971df2edd8c95
hoklenghak <hoklenghak168@gmail.com>
2023-01-05 19:08:11 +0700

    [art-card] blurhash height fixed

--- frontend/src/components/ArtCard/ArtCard.jsx
+++ frontend/src/components/ArtCard/ArtCard.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Blurhash } from "react-blurhash";
 
 import { HiUser } from "react-icons/hi2";
 import { HiOutlineHeart } from "react-icons/hi";
+import { useRef } from "react";
 
 const ArtCard = ({ art, artist, title, like }) => {
   const [isLoaded, setLoad] = useState(false);
 
+  const imgRef = useRef();
+
   if (art.rpath !== "" && art.preimage !== "")
     return (
       <div
-        className={`artcard--container relative rounded-md mb-6 sm:mb-6 aspect-[${
-          art.height + "/" + art.width
-        }] h-full`}
+        className={`artcard--container relative rounded-md mb-6 sm:mb-6 `}
+        // style={{
+        //   aspectRatio: `${art.height}/${art.width} `,
+        // }}
       >
         <img
+          ref={imgRef}
           src={art.tpath}
           alt={title}
           className={`rounded-md ${
@@ -27,27 +32,22 @@ const ArtCard = ({ art, artist, title, like }) => {
           draggable={false}
         />
 
-        <div className="artcard--info-container flex flex-col justify-between backdrop-brightness-90 duration-200 transition-all first-letter: opacity-0 hover:opacity-100 absolute top-0 left-0 w-full h-full p-5">
-          <section>
-            {/* <div className="flex flex-col gap-3 w-full">
+        <section className="artcard--info-container flex flex-col justify-between backdrop-brightness-90 duration-200 transition-all first-letter: opacity-0 hover:opacity-100 absolute top-0 left-0 w-full h-full p-5">
+          <div className="w-full h-full flex flex-col justify-between">
+            <div className="w-full flex justify-between content-center">
               <div className="flex gap-4">
-                <div className="p-2 bg-primary-90 rounded-lg text-secondary-100">
-                  <HiUser className="w-5 h-5" />
-                </div>
-                <span className="text-ellipsis text-primary-100 text-base text-center self-center">{`${artist.firstName} ${artist.lastName}`}</span>
+                <img src="" alt="" className="w-7 h-7" />
+                <span>username</span>
               </div>
-            </div> */}
-          </section>
-          <section className="relative bottom-0 left-0 flex">
-            <span className="text-ellipsis text-primary-100 text-base w-full self-center">
-              {title}
-            </span>
-            <button className="flex flex-col gap-2 w-1/5 bg-transparent outline-none border-none shadow-none">
-              <HiOutlineHeart className="w-5 h-5 text-primary-100" />
-              <span className=" text-primary-100 text-base">{like}</span>
-            </button>
-          </section>
-        </div>
+              <div className="w-full flex">
+                <div className="w-full flex flex-col gap-2">
+                  <span className="">{title}</span>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
         {!isLoaded && (
           <Blurhash
@@ -59,12 +59,13 @@ const ArtCard = ({ art, artist, title, like }) => {
             height={"100%"}
             className={"artcard--blurhash"}
             style={{
-              aspectRatio: `${art.width}/${art.height}`,
-              position: "absolute",
+              aspectRatio: `${art.width / art.height}`,
+              position: "relative",
               objectFit: "cover",
               top: "0",
               left: "0",
               display: "block",
+              minHeight: "17.5rem",
             }}
           />
         )}
