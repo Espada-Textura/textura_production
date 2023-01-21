b1541ad6fbe7c918f497b5cd974a8c99f2df468d
hoklenghak <hoklenghak168@gmail.com>
2023-01-21 22:23:21 +0700

    [notfound] ~ remove image

--- frontend/src/containers/NotFound/NotFound.jsx
+++ frontend/src/containers/NotFound/NotFound.jsx
@@ -16,7 +16,7 @@ const NotFound = () => {
   }
 
   return (
-    <div className="not-found-container flex items-center justify-center h-screen w-full md:px-52 lg:px-80">
+    <div className="not-found-container flex items-center justify-center h-full min-h-screen w-full md:px-52 lg:px-80">
       <div className="flex flex-col items-center justify-ceneter xl:flex-row xl:justify-between">
         <div className="xl:items-start xl:pr-40">
           <div className="text-6xl font-bold text-accent-100 my-3 text-center xl:text-left xl:my-8">
@@ -25,13 +25,13 @@ const NotFound = () => {
           <div className="text-4xl font-bold text-accent-100 my-3 text-center xl:text-left xl:my-5">
             Page Not Found!
           </div>
-          <div className="my-8 w-96 md:w-[500px] xl:hidden">
+          {/* <div className="my-8 w-96 md:w-[500px] xl:hidden">
             <img
               src={NotFoundBackgroundImage}
               alt="not-found"
               draggable="false"
             />
-          </div>
+          </div> */}
           <div className="text-base font-medium my-2 flex flex-col items-center justify-center xl:items-start xl:my-5">
             <div className="my-1">This page doesn't exist or was removed!</div>
             <div className="mb-1">We suggest you back to home.</div>
@@ -43,13 +43,13 @@ const NotFound = () => {
             Back to home
           </button>
         </div>
-        <div className="my-8 lg:w-[650px] xl:w-[750px] max-xl:hidden xl:pl-40">
+        {/* <div className="my-8 lg:w-[650px] xl:w-[750px] max-xl:hidden xl:pl-40">
           <img
             src={NotFoundBackgroundImage}
             alt="not-found"
             draggable="false"
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );
